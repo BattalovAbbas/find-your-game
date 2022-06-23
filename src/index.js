48bf16d2b5a6624ce9368701e2bb46d05c06ae78
abbasbattalov <abbasbattalov@yandex-team.ru>
2022-06-23 21:47:14 +0400

    try to show user name again

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@
         const name = document.getElementById('name');
         const photo = document.getElementById('photo');
         name.textContent = window.Telegram.WebApp.initDataUnsafe.user.first_name;
-        photo.src = window.Telegram.WebApp.initDataUnsafe.user.photo_url;
         console.log(window.Telegram.WebApp.initDataUnsafe);
     } else {
         console.error("window.Telegram.WebApp not found")
