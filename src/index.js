664107fec2f16e87adc661adc5cf142da96f76c6
Abbas <battalov.abbas.94@gmail.com>
2022-06-25 12:11:14 +0400

    update telegram check

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 (function () {
-    if (window.Telegram.WebApp) {
+    if (window.Telegram.WebApp.initDataUnsafe) {
         window.Telegram.WebApp.ready();
         const name = document.getElementById('name');
         const photo = document.getElementById('photo');
         name.textContent = window.Telegram.WebApp.initDataUnsafe.user.first_name;
         console.log(window.Telegram.WebApp.initDataUnsafe);
     } else {
-        console.error("window.Telegram.WebApp not found")
+        console.error("Telegram not found")
     }
 })();
